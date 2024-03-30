c5a3558b51c012d4b80426c6223d3081f4686f4f
PinakGupta <pinakgupta99@gmail.com>
2024-03-30 13:54:18 +0530

    Add contact landlord functionality to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -3,7 +3,9 @@ import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
+import { useSelector } from 'react-redux';
 import 'swiper/css/bundle';
+import Contact from '../components/Contact';
 import {
     FaBath,
     FaBed,
@@ -22,7 +24,9 @@ export default function Listing() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false); 
     const params = useParams();
+    const {currentUser} = useSelector((state) => state.user);
     useEffect(() => {
     const fetchListing = async () => {
     try {
@@ -44,7 +48,6 @@ export default function Listing() {
     };
     fetchListing();
     }, [params.listingId]);
-    console.log(loading);
 
     return (
     <main>
@@ -102,7 +105,7 @@ export default function Listing() {
                 </p>
                 {listing.offer && (
                     <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                    ${+listing.regularPrice - +listing.discountPrice}
+                    ${+listing.regularPrice - +listing.discountPrice} discount
                     </p>
                 )}
                 </div>
@@ -132,6 +135,12 @@ export default function Listing() {
                     {listing.furnished ? 'Furnished' : 'Unfurnished'}
                 </li>
                 </ul>
+                {currentUser && listing.userRef !== currentUser._id && !contact && (
+                    <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                        Contact landlord
+                    </button>
+                )}
+                {contact && <Contact listing={listing}/>}
             </div>
         </div>
     )}
