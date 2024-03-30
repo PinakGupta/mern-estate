ccdb9b80fe69712de8c7ec80f3f138b76e92e765
PinakGupta <pinakgupta99@gmail.com>
2024-03-30 11:54:02 +0530

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -300,7 +300,9 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                     <p>Regular price</p>
-                    <span className="text-xs">($ / month)</span>
+                    {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                    )}
                 </div>
                 </div>
 
@@ -319,7 +321,9 @@ export default function CreateListing() {
                             />
                         <div className="flex flex-col items-center">
                             <p>Discounted price</p>
-                            <span className="text-xs">($ / month)</span>
+                            {formData.type === 'rent' && (
+                                <span className='text-xs'>($ / month)</span>
+                            )}
                         </div>
                         </div>
                     )
